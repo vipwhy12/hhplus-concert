54a20cf2461e3a5a4aa0c71eeebf45cfc65b68b5
vipwhy12 <vipwhy12@naver.com>
2024-11-12 04:36:35 +0900

    fix: 토큰 키 생성 방식 수정

--- src/domain/token/dtos/waiting.token.dto.ts
+++ src/domain/token/dtos/waiting.token.dto.ts
@@ -1,12 +1,11 @@
-import { WAITING_TOKEN_KEY } from '../constants/token.constant';
-
 export class WaitingToken {
-  static readonly WAITING_TOKEN_KEY = WAITING_TOKEN_KEY;
   userId: number;
   date: Date;
+  tokenKey: string;
 
-  constructor(userId: number, date: Date) {
+  constructor(userId: number, date: Date, tokenKey: string) {
     this.userId = userId;
     this.date = date;
+    this.tokenKey = tokenKey;
   }
 }
--- src/domain/token/token.service.ts
+++ src/domain/token/token.service.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { TokenRepository, TokenRepositoryToken } from './token.repository';
 import { WaitingToken } from './dtos/waiting.token.dto';
+import { WAITING_TOKEN_KEY } from './constants/token.constant';
 
 @Injectable()
 export class TokenService {
@@ -10,7 +11,11 @@ export class TokenService {
   ) {}
 
   async issueWaitingToken(userId: number): Promise<number> {
-    const waitingToken = new WaitingToken(userId, new Date());
+    const waitingToken = new WaitingToken(
+      userId,
+      new Date(),
+      WAITING_TOKEN_KEY,
+    );
     const myToken = await this.tokenRepository.createWaitingToken(waitingToken);
 
     return myToken;
