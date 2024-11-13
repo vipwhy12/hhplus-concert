b0379edec4f0a596e0b77cc05a424b6ac7a555ce
vipwhy12 <vipwhy12@naver.com>
2024-11-14 04:46:11 +0900

    feat: Base Entity 구현

---
+++ src/common/entities/base.entity.ts
@@ -0,0 +1,22 @@
+import {
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class BaseEntity {
+  @PrimaryGeneratedColumn({ type: 'int' })
+  id: number;
+
+  /**
+   * 생성일 - 시, 분, 초가 포함된 `datetime` 형식
+   */
+  @CreateDateColumn({ type: 'datetime', comment: '생성일' })
+  createAt: Date;
+
+  /**
+   * 수정일 - 시, 분, 초가 포함된 `datetime` 형식
+   */
+  @UpdateDateColumn({ type: 'datetime', comment: '수정일' })
+  updatedAt: Date;
+}
