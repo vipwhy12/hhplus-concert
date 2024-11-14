913202cbc405d3b43e8899455be6bef1c710e108
vipwhy12 <vipwhy12@naver.com>
2024-11-15 06:27:34 +0900

    feat: 특정 콘서트 예약 가능 날짜 조회

--- src/domain/concert/concert.controller.ts
+++ src/domain/concert/concert.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ConcertService } from './concert.service';
 import { ConcertFindResponseDto } from './dtos/response/concert.find.response.dto';
 
@@ -12,5 +12,13 @@ export class ConcertController {
   @Get()
   async getConcerts(): Promise<ConcertFindResponseDto[]> {
     return await this.concertService.getConcerts();
+  }
+
+  /**
+   * 특정 콘서트의 예약 가능 날짜 조회
+   */
+  @Get(':concertId/sessions')
+  async reservableConcertSession(@Param('sessionId') sessionId: number) {
+    return await this.concertService.reservableConcertSession(sessionId);
   }
 }
