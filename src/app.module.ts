0db8f768504494153ff918aeb1866335823a794c
vipwhy12 <vipwhy12@naver.com>
2024-11-15 06:27:00 +0900

    fix: typeOrm Entity 설정 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,16 @@ import { InfrastructureModule } from './infrastructure/infrastructure.module';
 
 import TypeOrmConfig from './common/config/type.orm.config';
 import RedisConfig from 'src/common/config/redis.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      cache: false,
       load: [TypeOrmConfig, RedisConfig],
     }),
+    TypeOrmModule.forRootAsync(TypeOrmConfig.asProvider()),
     DomainModule,
     InfrastructureModule,
   ],
