a1ff185db5818c130a26a041ae360c78b5b8a197
vipwhy12 <vipwhy12@naver.com>
2024-11-15 06:22:50 +0900

    feat: 더미 데이터 생성 구축

--- src/infrastructure/concert/concert.repository.impl.ts
+++ src/infrastructure/concert/concert.repository.impl.ts
@@ -1,15 +1,36 @@
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ConcertRepository } from 'src/domain/concert/concert.repository';
 import { ConcertEntity } from 'src/domain/concert/entities/concert.entity';
+import { ConcertSessionEntity } from 'src/domain/concert/entities/concert.session.entity';
 import { Repository } from 'typeorm';
 
+@Injectable()
 export class ConcertRepositoryImpl implements ConcertRepository {
   constructor(
     @InjectRepository(ConcertEntity)
     private readonly concert: Repository<ConcertEntity>,
+    @InjectRepository(ConcertSessionEntity)
+    private readonly concertSessions: Repository<ConcertSessionEntity>,
   ) {}
+
+  insert(
+    concert: Record<string, import('nestjs-seeder').FactoryValue>[],
+  ): Promise<any> {
+    return this.concert.save(concert);
+  }
+
+  delete(arg0: any): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
 
   async getConcerts(): Promise<ConcertEntity[]> {
     return this.concert.find();
+  }
+
+  async getReservableConcertSession(concertId: number) {
+    return this.concertSessions.find({
+      where: { concertId },
+    });
   }
 }
