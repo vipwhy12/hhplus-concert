9fae35756d538418a8af4908966761862edbc712
vipwhy12 <vipwhy12@naver.com>
2024-11-15 23:02:27 +0900

    chore: reservation 모듈 생성

--- src/domain/domain.module.ts
+++ src/domain/domain.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TokenModule } from './token/token.module';
 import { ConcertModule } from './concert/concert.module';
 import { UserModule } from './user/user.module';
+import { ReservationModule } from './reservation/reservation.module';
 
 @Module({
-  imports: [TokenModule, ConcertModule, UserModule],
+  imports: [TokenModule, ConcertModule, UserModule, ReservationModule],
 })
 export class DomainModule {}
