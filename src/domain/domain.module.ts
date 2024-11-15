88499a1541f5cc8825ae54b5198ac9536c1b5e41
vipwhy12 <vipwhy12@naver.com>
2024-11-15 17:01:13 +0900

    feat: 사용자 모듈 추가

--- src/domain/domain.module.ts
+++ src/domain/domain.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TokenModule } from './token/token.module';
 import { ConcertModule } from './concert/concert.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TokenModule, ConcertModule],
+  imports: [TokenModule, ConcertModule, UserModule],
 })
 export class DomainModule {}
