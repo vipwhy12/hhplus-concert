5817edcb300972640af71191decdef9ea1f5675e
vipwhy12 <vipwhy12@naver.com>
2024-11-15 22:41:29 +0900

    feat: 포인트 충전 구현

--- src/domain/user/points/points.service.ts
+++ src/domain/user/points/points.service.ts
@@ -1,15 +1,33 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { POINTS_REPOSITORY, PointsRepository } from './points.repository';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class PointsService {
   constructor(
     @Inject(POINTS_REPOSITORY)
     private readonly pointsRepository: PointsRepository,
+    private readonly dataSource: DataSource,
   ) {}
 
   async getMyPoint(id: string) {
     const userId = Number(id);
-    return await this.pointsRepository.findPointByUserId(userId);
+    return await this.pointsRepository.findByUserId(userId);
+  }
+
+  async charge(id: string, balance: number) {
+    const userId = Number(id);
+    const updateBalance = await this.dataSource.transaction(async (manager) => {
+      const myPoint = await this.pointsRepository.findByUserId(userId, manager);
+      const updatedBalance = myPoint.balance + balance;
+
+      return await this.pointsRepository.update(
+        userId,
+        updatedBalance,
+        manager,
+      );
+    });
+
+    return updateBalance;
   }
 }
