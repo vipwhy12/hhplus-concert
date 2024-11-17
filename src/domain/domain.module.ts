1f3e9cbf6e193a3932ce14f915a3fa2841d8e973
vipwhy12 <vipwhy12@naver.com>
2024-11-17 18:43:00 +0900

    feat: 도메인 모듈에 포인트 모듈 import

--- src/domain/domain.module.ts
+++ src/domain/domain.module.ts
@@ -3,8 +3,15 @@ import { TokenModule } from './token/token.module';
 import { ConcertModule } from './concert/concert.module';
 import { UserModule } from './user/user.module';
 import { ReservationModule } from './reservation/reservation.module';
+import { PointsModule } from './points/points.module';
 
 @Module({
-  imports: [TokenModule, ConcertModule, UserModule, ReservationModule],
+  imports: [
+    TokenModule,
+    ConcertModule,
+    UserModule,
+    ReservationModule,
+    PointsModule,
+  ],
 })
 export class DomainModule {}
