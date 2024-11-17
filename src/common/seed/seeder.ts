a2530fe7acdfb3dcf73cdef3c9bf419d17d84663
vipwhy12 <vipwhy12@naver.com>
2024-11-17 18:41:25 +0900

    fix: seeder 전체 모듈 추가

--- src/common/seed/seeder.ts
+++ src/common/seed/seeder.ts
@@ -1,13 +1,15 @@
+import TypeOrmConfig from 'src/common/config/type.orm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { seeder } from 'nestjs-seeder';
 import { ConcertModule } from 'src/domain/concert/concert.module';
-import { ConcertSeeder } from './seeder.concert';
-import TypeOrmConfig from 'src/common/config/type.orm.config';
 import { ConfigModule } from '@nestjs/config';
-import { UserSeeder } from './seeder.user';
 import { UserModule } from 'src/domain/user/user.module';
 import { PointSeeder } from './seeder.point';
 import { PointsModule } from 'src/domain/points/points.module';
+import { ReservationModule } from 'src/domain/reservation/reservation.module';
+import { UserSeeder } from './seeder.user';
+import { ConcertSeeder } from './seeder.concert';
+import { ReservationSeeder } from './seeder.reservation';
 
 seeder({
   imports: [
@@ -20,7 +22,6 @@ seeder({
     ConcertModule,
     UserModule,
     PointsModule,
+    ReservationModule,
   ],
-}).run([PointSeeder]);
-
-// }).run([UserSeeder, ConcertSeeder]);
+}).run([UserSeeder, ConcertSeeder, PointSeeder, ReservationSeeder]);
