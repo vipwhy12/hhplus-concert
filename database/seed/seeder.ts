145a3553d48f0f3de455b7637141f2303d21914e
vipwhy12 <vipwhy12@naver.com>
2024-11-18 04:30:27 +0900

    fix: seeder 부분 모듈 삭제

--- database/seed/seeder.ts
+++ database/seed/seeder.ts
@@ -1,17 +1,15 @@
 import TypeOrmConfig from 'src/common/config/type.orm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { seeder } from 'nestjs-seeder';
-import { ConcertModule } from 'src/domain/concert/concert.module';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from 'src/domain/user/user.module';
-import { PointsModule } from 'src/domain/points/points.module';
-import { ReservationModule } from 'src/domain/reservation/reservation.module';
 import { UsersEntity } from 'src/domain/user/entities/user.entity';
 import { ReservationEntity } from 'src/domain/reservation/entities/reservation.entity';
 import { PointSeeder } from './point/point.seeder';
 import { PointEntity } from 'src/domain/points/entities/point.entity';
 import { ConcertEntity } from 'src/domain/concert/entities/concert.entity';
 import { ConcertSeeder } from './concert/concert.seeder';
+import { UserSeeder } from './user/user.seeder';
+import { ReservationSeeder } from './reservation/reservation.seeder';
 
 seeder({
   imports: [
@@ -27,14 +25,5 @@ seeder({
       PointEntity,
       ConcertEntity,
     ]),
-    ConcertModule,
-    UserModule,
-    PointsModule,
-    ReservationModule,
   ],
-}).run([
-  // UserSeeder,
-  // ReservationSeeder,
-  // PointSeeder,
-  ConcertSeeder,
-]);
+}).run([UserSeeder, ReservationSeeder, PointSeeder, ConcertSeeder]);
