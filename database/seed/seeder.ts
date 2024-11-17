ee3b6b659ef2c026aafddc21edc437fca34f973b
vipwhy12 <vipwhy12@naver.com>
2024-11-18 03:07:39 +0900

    refactor: base 모듈 사용

--- database/seed/seeder.ts
+++ database/seed/seeder.ts
@@ -6,11 +6,12 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from 'src/domain/user/user.module';
 import { PointsModule } from 'src/domain/points/points.module';
 import { ReservationModule } from 'src/domain/reservation/reservation.module';
-import { UserSeeder } from './user/seeder.user';
 import { ConcertSeeder } from './concert/seeder.concert';
 import { ConcertSessionSeeder } from './concert/seeder.concert.session';
 import { PointSeeder } from './point/seeder.point';
 import { ReservationSeeder } from './reservation/seeder.reservation';
+import { UserSeeder } from './user/user.seeder';
+import { UsersEntity } from 'src/domain/user/entities/user.entity';
 
 seeder({
   imports: [
@@ -20,6 +21,7 @@ seeder({
       load: [TypeOrmConfig],
     }),
     TypeOrmModule.forRootAsync(TypeOrmConfig.asProvider()),
+    TypeOrmModule.forFeature([UsersEntity]),
     ConcertModule,
     UserModule,
     PointsModule,
