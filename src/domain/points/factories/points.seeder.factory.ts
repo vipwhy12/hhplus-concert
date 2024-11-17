59be31c01478aec02f407cf070defde880187c02
vipwhy12 <vipwhy12@naver.com>
2024-11-18 03:10:27 +0900

    chore: 테스트 파일 삭제

--- src/domain/points/factories/points.seeder.factory.ts
+++
@@ -1,6 +0,0 @@
-import { Faker } from '@faker-js/faker';
-import { PointEntity } from '../entities/point.entity';
-
-// export PointSeederFactory(PointerEvent, facker: Faker){
-//   const point = new PointEntity();
-// }
