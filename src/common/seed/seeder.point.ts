7086a2d48b27569f4628faa1813602bef5fa4e5b
vipwhy12 <vipwhy12@naver.com>
2024-11-17 18:31:11 +0900

    feat: 포인트 더미데이터 생성 로직 추가

--- src/common/seed/seeder.point.ts
+++ src/common/seed/seeder.point.ts
@@ -17,10 +17,25 @@ export class PointSeeder implements Seeder {
   ) {}
 
   async seed() {
-    const dataCount = this.configService.get<number>('SEED_DATA_COUNT');
-    const point = DataFactory.createForClass(PointEntity).generate(dataCount);
+    const totalCount = this.configService.get<number>('SEED_DATA_COUNT'); // 총 데이터 개수
+    const batchSize = 1000; // 한 번에 처리할 데이터 개수
+    const totalBatches = Math.ceil(totalCount / batchSize); // 필요한 배치 수 계산
 
-    await this.pointReservation.insert(point);
+    for (let batchIndex = 0; batchIndex < totalBatches; batchIndex++) {
+      // 현재 배치에 포함될 데이터 개수 계산
+      const currentBatchSize = Math.min(
+        batchSize,
+        totalCount - batchIndex * batchSize,
+      );
+
+      // 현재 배치 데이터 생성
+      const batchData =
+        DataFactory.createForClass(PointEntity).generate(currentBatchSize);
+
+      // 데이터 삽입
+      await this.pointReservation.insert(batchData);
+      console.log(`Batch ${batchIndex + 1}/${totalBatches} inserted`);
+    }
   }
 
   drop() {
--- src/domain/points/entities/point.entity.ts
+++ src/domain/points/entities/point.entity.ts
@@ -14,7 +14,7 @@ export class PointEntity extends BaseEntity {
   @Factory(() => {
     let id;
     do {
-      id = Math.floor(Math.random() * 10000); // 랜덤 ID 생성
+      id = Math.floor(Math.random() * 100000000); // 랜덤 ID 생성
     } while (userIdSet.has(id)); // 중복 여부 확인
     userIdSet.add(id); // 고유 값으로 추가
     return id;
