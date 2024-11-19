e6a687eb93eeb9c98a92147afa64bb2bad9e3609
vipwhy12 <vipwhy12@naver.com>
2024-11-20 05:49:01 +0900

    feat: 결제 구현

--- src/domain/payments/payments.facade.ts
+++ src/domain/payments/payments.facade.ts
@@ -1,10 +1,45 @@
+import { TokenService } from './../token/token.service';
+import { PointsService } from './../points/points.service';
 import { Injectable } from '@nestjs/common';
+import { ReservationService } from '../reservation/reservation.service';
+import { PaymentsService } from './payments.service';
+import { UserService } from '../user/user.service';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class PaymentsFacade {
-  constructor() {}
+  constructor(
+    private readonly datasoure: DataSource,
+    private readonly userService: UserService,
+    private readonly paymentsService: PaymentsService,
+    private readonly pointsService: PointsService,
+    private readonly reservationService: ReservationService,
+    private readonly TokenService: TokenService,
+  ) {}
 
-  precessPayment() {
-    //1. 결제 할 꺼야
+  async capture(userId: string, reservationId: string, amount: number) {
+    await this.datasoure.transaction(async (manager) => {
+      const reservation = await this.reservationService.getReservationById(
+        reservationId,
+        manager,
+      );
+
+      const user = await this.userService.getUserById(userId, manager);
+
+      if (user.balance < amount) throw new Error('잔액이 부족합니다');
+
+      //3. 결제 금액 차감
+      await this.pointsService.updateBalance(userId, amount, manager);
+
+      //4. 결제 정보 저장
+      await this.paymentsService.createPayment(
+        userId,
+        reservationId,
+        amount,
+        manager,
+      );
+
+      //5. 대기열 토큰 만료
+    });
   }
 }
--- src/domain/payments/payments.service.ts
+++ src/domain/payments/payments.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { EntityManager } from 'typeorm';
+import { PAYMENTS_REPOSITORY, PaymentsRepository } from './payments.repository';
 
 @Injectable()
 export class PaymentsService {
-  capture() {
-    throw new Error('Method not implemented.');
+  constructor(
+    @Inject(PAYMENTS_REPOSITORY)
+    private readonly paymentRepository: PaymentsRepository,
+  ) {}
+
+  async createPayment(
+    userId: string,
+    reservationId: string,
+    amount: number,
+    manager?: EntityManager,
+  ) {
+    await this.paymentRepository.createPayment(
+      userId,
+      reservationId,
+      amount,
+      manager,
+    );
   }
-  constructor() {}
 }
